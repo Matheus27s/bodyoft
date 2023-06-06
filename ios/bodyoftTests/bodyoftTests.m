d229c89b20923bbf7ce5e7aa2deaca5409fe3999
Matheus Sousa <mssantos19@stefanini.com>
2023-06-06 16:35:37 -0300

    adicionado configuracoes de rename

--- ios/bodyoftTests/bodyoftTests.m
+++ ios/bodyoftTests/bodyoftTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface bodyfitTests : XCTestCase
+@interface bodyoftTests : XCTestCase
 
 @end
 
-@implementation bodyfitTests
+@implementation bodyoftTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
